597932647f4956d2aba369de6689d038d6748ff4
Ihor Pysarenko <ihor.pysarenk@abstract.it>
2026-01-22 11:54:32 +0200

    fix change theme icon on reload

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -2,12 +2,14 @@
   <ClientOnly v-if="!colorMode?.forced">
     <div class="flex items-center justify-center pt-4">
       <UButton
-        :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
         color="neutral"
         variant="ghost"
         :aria-label="`Switch to ${isDark ? 'light' : 'dark'} mode`"
         @click="isDark = !isDark"
-      />
+      >
+        <UIcon v-show="!isDark" name="i-lucide-moon" size="24" />
+        <UIcon v-show="isDark" name="i-lucide-sun" size="24" />
+      </UButton>
     </div>
     <template #fallback>
       <div class="size-8" />
